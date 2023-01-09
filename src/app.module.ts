51fc847a4e76be1d72777d628464bc979d130075
Bhumi <nagbhumi02@gmail.com>
2023-01-09 23:24:54 +0530

    myfiles

---
+++ src/app.module.ts
@@ -0,0 +1,15 @@
+import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { StudentController } from './controller/student.controller';
+import { StudentSchema } from './schema/student.schema';
+import { StudentService } from './service/student.service';
+@Module({
+  imports: [MongooseModule.forRoot('mongodb+srv://bhumi02:Lldq1gK03cWlGYPd@cluster0.uuq82ib.mongodb.net/test',{dbName: 'studentdb'}),
+  MongooseModule.forFeature([{ name: 'Student', schema: StudentSchema }])
+],
+  controllers: [AppController,StudentController],
+  providers: [AppService,StudentService],
+})
+export class AppModule {}
